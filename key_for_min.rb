6d70b477972aa5b7893afda84ef05b56160e09de
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-01 04:28:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@ def key_for_min_value(name_hash)
 name_hash= {"lowest_key"=>"nil"}
 
 name_hash.each do |key,val|
-   if val 
-    array a<=>b 
+   if val < name_hash["lowest_key"]
+    
     end
   end 
 name_hash[]
